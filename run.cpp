762966c8dc337b222f889e0509e18ef4cd9a355d
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2019-09-13 12:39:40 +0200

    Fix loglevel

--- run.cpp
+++ run.cpp
@@ -6,7 +6,7 @@
 #include <networkit/io/NetworkitBinaryReader.hpp>
 
 int main(int argc, char *argv[]) {
-    Aux::Log::Settings::setLogLevel(Aux::Log::LogLevel::quiet);
+    Aux::Log::Settings::setLogLevel(Aux::Log::LogLevel::error);
     auto g = NetworKit::NetworkitBinaryReader().read(argv[1]).toUnweighted();
     Aux::Timer timer;
     NetworKit::KadabraBetweenness kb(g, 0.01, 0.1, argv[2][6]);
