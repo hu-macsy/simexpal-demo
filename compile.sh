e9182793b1d4c771dc1e01f800d48df583bf9f3b
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2019-09-13 12:28:15 +0200

    Update compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-g++-9 run.cpp $(pkg-config --cflags networkit) $(pkg-config --libs networkit) -o run -O3
+#!/bin/bash
+g++-9 $SRCDIR/run.cpp $(pkg-config --cflags networkit) $(pkg-config --libs networkit) -o run -O3
