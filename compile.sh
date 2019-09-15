fd2d963a4306a8fae8876591118b347fe42807f4
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2019-09-15 17:57:23 +0200

    Copy run script to prefix dir

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-g++-9 $SRCDIR/run.cpp $(pkg-config --cflags networkit) $(pkg-config --libs networkit) -std=c++11 -o $SRCDIR/run -O3
+g++-9 $SRCDIR/run.cpp $(pkg-config --cflags networkit) $(pkg-config --libs networkit) -std=c++11 -o run -O3
+mkdir -p $PREFIXDIR/bin
+cp run $PREFIXDIR/bin
+
