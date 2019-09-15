d5147596d42af14731ff7124a33565e0de1dba41
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2019-09-15 15:19:54 +0200

    Write executable to srcdir

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++-9 $SRCDIR/run.cpp $(pkg-config --cflags networkit) $(pkg-config --libs networkit) -std=c++11 -o run -O3
+g++-9 $SRCDIR/run.cpp $(pkg-config --cflags networkit) $(pkg-config --libs networkit) -std=c++11 -o $SRCDIR/run -O3
